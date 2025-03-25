efb13f2f7255efc11d15d5fa6fa9a5676f274b79
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-03-25 23:46:42 +0100

    create GET and POST endpoints

--- Program.cs
+++ Program.cs
@@ -1,6 +1,54 @@
+using GameStore.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+const string GetGameEndpointName = "GetGame";
+
+List<GameContract> games = [
+    new (
+            1,
+            "FIFA 2025",
+            "Football",
+            29.99M,
+            new DateOnly(1999, 3, 22)
+        ),
+    new (
+            2,
+            "Street Fighter",
+            "Fighting",
+            9.99M,
+            new DateOnly(1995, 5, 21)
+        ),
+    new (
+            3,
+            "Need For Speed",
+            "Racing",
+            59.99M,
+            new DateOnly(2004, 9, 13)
+        ),
+    ];
+
+// GET /games
+app.MapGet("/games", () => games);
+
+// GET /games/{id}
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndpointName);
+
+// POST /games
+app.MapPost("/games", (CreateGameContract newGame) =>
+{
+    GameContract game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+
+    games.Add( game );
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
 
 app.Run();
